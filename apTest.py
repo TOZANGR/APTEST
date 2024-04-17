855d7a18174b5fad045953b6e0e7123aa5d25a75
TOZANGR <131818530+TOZANGR@users.noreply.github.com>
2024-04-17 12:33:49 -0700

    Add files via upload

--- apTest.py
+++ apTest.py
@@ -2,16 +2,23 @@ from tkinter import *
 import overpy
 api = overpy.Overpass()
 root = Tk()
-root.title("Bingus")
-root.geometry('350x200')
-lbl = Label(root, text = "Please enter")
+root.title("App")
+root.geometry('510x200')
+lbl = Label(root, text = "Please enter a housenumber")
 lbl.grid()
 txt = Entry(root, width=10)
 txt.grid(column =1, row =0)
+lbl1 = Label(root, text = "Please enter an area(city, state, country, province, ect)")
+lbl1.grid()
+txt1 = Entry(root, width=10)
+txt1.grid(column =1, row =1)
+results = Label(root, text= "results: ")
+results.grid(column=0, row=3)
 def clicked():
    value = txt.get()
+   area = txt1.get()
    result = api.query("""
-   area[name = "Portland"]->.a;
+   area[name = """ + area + """]->.a;
   (
   node["addr:housenumber" = """ + value + """](area.a);
   way["addr:housenumber" = """ + value + """](area.a);
@@ -20,15 +27,52 @@ def clicked():
   out center;
    """)
    coords  = []
-   coords += [node.lat
+   coords += [node.tags
           for node in result.nodes]
-   coords += [way.id
+   coords += [way.tags
           for way in result.ways]
-   coords += [relation.id
+   coords += [relation.tags
           for relation in result.relations]
-   print(coords)
-btn = Button(root, text = "Click me" ,
+   addr = []
+   final = ''
+   for i in coords:
+      if i.get("addr:housenumber"):
+         final += i.get('addr:housenumber')
+         final += ' ' 
+         print(i)
+         if i.get("addr:street"):
+              final += i.get('addr:street') 
+              final += ' '
+              if i.get("addr:city"):
+                     final += ' '
+                     final += i.get('addr:city') 
+                     if i.get("addr:postcode"):
+                            final += ' '
+                            final += i.get('addr:postcode')   
+                            if i.get("addr:state"):
+                                   final += ' '
+                                   final += i.get('addr:state')
+                            if i.get("addr:country"):
+                                 final += ' '
+                                 final += i.get('addr:country')
+      print(final, '\n')
+      addr.append(final)
+      final = ''
+   j = 0
+   x = 0
+   global root
+   root.destroy()
+   root1 = Tk()
+   for i in range(len(addr)):
+        x += 1
+        addressLabel = Label(root1, text=addr[i])
+        addressLabel.grid(column=j, row=(x + 4))
+        root1.geometry(str(300 * (j + 1)) + 'x' + str(25 * i))
+        if x > 44:
+             j += 1
+             x = 0
+   root1.mainloop()           
+btn = Button(root, text = "Enter" ,
             fg = "red", command=clicked)
-btn.grid(column=2, row=0)
+btn.grid(column=2, row=1)
 root.mainloop()
-      
